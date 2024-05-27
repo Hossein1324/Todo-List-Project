b3a21a0169d70341e1f98fa32481f3cffa49bd0b
Hossein <hosseinpro@gmail.com>
2024-05-27 15:16:33 +0430

    done with first js code

--- TodoList.js
+++ TodoList.js
@@ -5,3 +5,27 @@ const todoList = document.querySelector('.todo-list');
 todoButton.addEventListener('click', addTodo);
 
 
+
+function addTodo(event) {
+    event.preventDefault();
+// creat div item of ul
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo')
+// creat li inside of the div
+     const newTodo = document.createElement('li');
+     newTodo.innerText = 'hey';
+     newTodo.classList.add('todo-item');
+     todoDiv.appendChild(newTodo);
+    //  creat check button
+    const checkButton = document.createElement('button');
+    checkButton.innerText = 'check';
+    checkButton.classList.add('check-btn');
+    todoDiv.appendChild(checkButton);
+        //  creat delete button
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'delete';
+        deleteButton.classList.add('delete-btn');
+        todoDiv.appendChild(deleteButton);
+        // append to ul tag
+        todoList.appendChild(todoDiv);
+}
